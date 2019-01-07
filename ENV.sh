4fbf50667472047ecaa1b4a62aae5d329860e6fe
Joost van Someren <joost.v.someren@gmail.com>
2019-01-07 22:04:45 +0100

    Add environment file

---
+++ ENV.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get your API key from https://www.cloudflare.com/a/account/my-account
+API_KEY="your-api-key"
+EMAIL="your.email@example.com"
+DOMAINS=(
+  "*.example.com,example.com"
+  "*.another-example.com.another-example.com"
+)
