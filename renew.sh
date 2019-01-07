faaa4c97f24d4f3cff14acbeb36fac5e0b56c139
Joost van Someren <joost.v.someren@gmail.com>
2019-01-07 22:41:11 +0100

    Add root renew script

---
+++ renew.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get the absolute path to the directory of this script
+# code taken from https://gist.github.com/TheMengzor/968e5ea87e99d9c41782
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# get additional environment variables
+source $DIR/ENV.sh
+
+count=0
+while [ "x${DOMAINS[count]}" != "x" ]
+do
+  echo "/usr/bin/certbot certonly --manual --preferred-challenges=dns --email joost.v.someren@gmail.com --manual-auth-hook $DIR/authenticator.sh --manual-cleanup-hook $DIR/cleanup.sh -d ${DOMAINS[count]}"
+  count=$(( $count + 1 ))
+done
