01f9feba94aae0044d0dd1a7403d6d02fd99c3d9
Joost van Someren <joost.v.someren@gmail.com>
2019-01-08 20:06:48 +0100

    Fix dot instead of comma

--- ENV.sh
+++ ENV.sh
@@ -5,5 +5,5 @@ API_KEY="your-api-key"
 EMAIL="your.email@example.com"
 DOMAINS=(
   "*.example.com,example.com"
-  "*.another-example.com.another-example.com"
+  "*.another-example.com,another-example.com"
 )
