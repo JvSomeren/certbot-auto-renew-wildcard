60ee635f26d286897ab49e4aca004df8346cf0d8
Joost van Someren <joost.v.someren@gmail.com>
2019-01-08 20:02:54 +0100

    Add sourcing of environment variable to hook files

--- authenticator.sh
+++ authenticator.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# get the absolute path to the directory of this script
+# code taken from https://gist.github.com/TheMengzor/968e5ea87e99d9c41782
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# get additional environment variables
+source $DIR/ENV.sh
+
 # Strip only the top domain to get the zone id
 DOMAIN=$(expr "$CERTBOT_DOMAIN" : '\(.*\..*\)')
 
